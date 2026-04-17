1263c5840164f8f44295423b5d1c8238ce89b919
Karan Gathani <karan.gathani@posit.co>
2026-04-16 22:51:32 -0700

    chore: remove temporary fix scripts

--- fix2.py
+++
@@ -1,8 +0,0 @@
-import sys
-with open('src/App.tsx', 'r') as f:
-    text = f.read()
-
-text = text.replace("formatter={(value: number) =>", "formatter={(value: any) =>")
-
-with open('src/App.tsx', 'w') as f:
-    f.write(text)
